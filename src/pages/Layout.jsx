26d1cd97a3b9e77e48079b40d7384f75da1ea85d
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-05-23 15:15:39 +0900

    feat: layout

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router';
+import { Link, NavLink, Outlet } from 'react-router';
 
 // 1. Outlet을 사용하여 모든 라우팅에서 보이게 해야합니다.
 // 2. 각 Link를 연동하여 알맞는 주소로 전송 해야 합니다.
@@ -13,12 +13,14 @@ function Layout() {
       </header>
       <div className="admin-content">
         <div className="sidebar">
-          <NavLink to="/admin">대시보드</NavLink>
-          <NavLink to="/admin/users">사용자</NavLink>
-          <NavLink to="/admin/settings">설정</NavLink>
-          <NavLink to="/admin/asdjnkobsdfjkl">잘못된 페이지</NavLink>
+          <Link to="/admin">대시보드</Link>
+          <Link to="/admin/users">사용자</Link>
+          <Link to="/admin/settings">설정</Link>
+          <Link to="/admin/asdjnkobsdfjkl">잘못된 페이지</Link>
         </div>
-        <div className="main-content"></div>
+        <div className="main-content">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
